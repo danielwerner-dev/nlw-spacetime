161f44ae69d09d33dc28c77425001a0fa3abc192
danielortiz3 <you@example.com>
2023-05-25 10:49:27 -0300

    feat: web adjustments following figma

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,11 +6,13 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div className=" flex-cols relative flex items-start justify-between overflow-hidden border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-2 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
         {/* Blur */}
-        <div className=" absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
         {/*  SignIn */}
         <a
           href=""
@@ -27,8 +29,38 @@ export default function Home() {
         </a>
 
         {/* Hero */}
-        <div>
-          <Image src={nlwLogo} alt="Nlw Spacetime" />
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
         </div>
       </div>
 
